58795971f0683f57be6780239bd43cfc658f5088
erzajuan <direkal2@gmail.com>
2021-03-07 21:48:07 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+    print("1. Plus (+)")
+    print("2. Minus (-)")
+    print("3. Time (x)")
+    print("4. Divide (/)")
+    choose = int(input("Your Choose  : "))
+    x = float(input("First Number     : "))
+    y = float(input("Second Number    : "))
+    if choose == 1:
+        print("Result : {}".format(x+y))
+    if choose == 2:
+        print("Result : {}".format(x-y))
+    if choose == 3:
+        print("Result : {}".format(x*y))
+    if choose == 4:
+        print("Result : {}".format(x/y))
+
+
+if __name__ == '__main__':
+    main()
